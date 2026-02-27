d90666a8281a8a23f19e37f2801aae1194799951
CLabEric <coderlabny@gmail.com>
2026-02-27 13:32:51 -0500

    initial search setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://recipecards.kitchen",
-  integrations: [sitemap()],
+  integrations: [sitemap(), pagefind()],
   vite: {
     plugins: [tailwindcss()]
   },
